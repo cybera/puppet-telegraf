6195b1b9c1ef0affa6f3af90f8564d381920081e
tatianameleshko <tatiana.meleshko@gmail.com>
2017-02-14 09:13:30 -0700

    Update install.pp
    updated init.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -17,10 +17,9 @@ class telegraf::install {
           location => "https://repos.influxdata.com/${_operatingsystem}",
           release  => $::lsbdistcodename,
           repos    => $::telegraf::repo_type,
-          key      => {
-            'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-            'source' => 'https://repos.influxdata.com/influxdb.key',
-          },
+          key      => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
+          key_source => 'https://repos.influxdata.com/influxdb.key',
+          include_src => false,
         }
         Class['apt::update'] -> Package['telegraf']
       }
